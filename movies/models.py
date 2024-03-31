416c246abc055a5ba72a8561dcf14d44c19598ac
journnie <jhyun095@gmail.com>
2024-03-31 11:29:39 +0900

    movie models

--- movies/models.py
+++ movies/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model): # 클래스 명은 테이블 이름
     # 각 변수 명은 필드 명
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=20)
     content = models.TextField()
     is_open = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
