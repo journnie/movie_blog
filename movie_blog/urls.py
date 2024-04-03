d34667aa0803e103935a641210f3b2869eba718f
journnie <jhyun095@gmail.com>
2024-04-03 11:28:43 +0900

    movie urls

--- movie_blog/urls.py
+++ movie_blog/urls.py
@@ -22,5 +22,6 @@ from accounts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('reviews/', include('reviews.urls'))
 ]
